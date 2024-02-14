34b99747b3f0db1834e458b5ee72f3708ecec8bc
Pascal Thomet <pthomet@gmail.com>
2024-02-14 18:45:27 +0100

    CMake

--- cmake/fetch_imgui_bundle.cmake
+++ cmake/fetch_imgui_bundle.cmake
@@ -1,17 +1,14 @@
-##########################################################
-# Prepare imgui_bundle during configure time
-##########################################################
-# Download imgui_bundle
-include(FetchContent)
-Set(FETCHCONTENT_QUIET FALSE)
-FetchContent_Declare(
-    imgui_bundle
-    GIT_REPOSITORY https://github.com/pthom/imgui_bundle.git
-    GIT_PROGRESS TRUE
-    # Enter the desired git tag below
-    GIT_TAG dev
-)
-FetchContent_MakeAvailable(imgui_bundle)
-# Make cmake function `imgui_bundle_add_app` available
-list(APPEND CMAKE_MODULE_PATH ${IMGUIBUNDLE_CMAKE_PATH})
-include(imgui_bundle_add_app)
+# 1/  Option 1: if you added imgui_bundle in a subfolder, you can add it to your project with:
+if(IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/external/imgui_bundle)
+    add_subdirectory(external/imgui_bundle)
+endif()
+
+## 2/  Option 2: simply fetch imgui_bundle during the build
+if (NOT TARGET imgui_bundle)
+    message(STATUS "Fetching imgui_bundle...")
+    include(FetchContent)
+    Set(FETCHCONTENT_QUIET FALSE)
+    FetchContent_Declare(imgui_bundle GIT_REPOSITORY https://github.com/pthom/imgui_bundle.git GIT_TAG main)
+    FetchContent_MakeAvailable(imgui_bundle)
+    set(IMMVISION_FETCH_OPENCV ON)
+endif()
