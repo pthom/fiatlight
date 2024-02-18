3e5053baff11cbde8a4df42bcc71372506de7720
Pascal Thomet <pthomet@gmail.com>
2024-02-18 09:34:38 +0100

    Add config

---
+++ src/python/fiatlight/config.py
@@ -0,0 +1,12 @@
+from imgui_bundle import ImVec4
+
+
+class FiatLightColors:
+    error = ImVec4(1.0, 0.6, 0.3, 1)
+
+
+class FiatlightConfig:
+    colors = FiatLightColors()
+
+
+config = FiatlightConfig()
