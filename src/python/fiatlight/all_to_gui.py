9a131f5b8395a7e5a3a7495c71cdb0cc9ab92699
Pascal Thomet <pthomet@gmail.com>
2024-02-23 18:05:29 +0100

    add all_to_gui

---
+++ src/python/fiatlight/all_to_gui.py
@@ -0,0 +1,25 @@
+from fiatlight.to_gui import TypeToGuiInfo
+from fiatlight.data_presenters import (
+    make_int_with_gui,
+    make_str_with_gui,
+    make_bool_with_gui,
+    make_float_with_gui,
+    IntWithGuiParams,
+    StrWithGuiParams,
+    BoolWithGuiParams,
+    FloatWithGuiParams,
+)
+
+from typing import List
+
+
+_ALL_TYPE_TO_GUI_INFO: List[TypeToGuiInfo] = [
+    TypeToGuiInfo(int, make_int_with_gui, IntWithGuiParams()),
+    TypeToGuiInfo(float, make_float_with_gui, FloatWithGuiParams()),
+    TypeToGuiInfo(str, make_str_with_gui, StrWithGuiParams()),
+    TypeToGuiInfo(bool, make_bool_with_gui, BoolWithGuiParams()),
+]
+
+
+def all_type_to_gui_info() -> List[TypeToGuiInfo]:
+    return _ALL_TYPE_TO_GUI_INFO
