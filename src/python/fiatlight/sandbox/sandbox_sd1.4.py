a8255c974a8d6734d68623a8e1e1542d44e42997
Pascal Thomet <pthomet@gmail.com>
2024-03-10 14:47:14 +0100

    Add sandboxes / Stable diffusion

---
+++ src/python/fiatlight/sandbox/sandbox_sd1.4.py
@@ -0,0 +1,15 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+model_id = "CompVis/stable-diffusion-v1-4"
+# device = "cuda"
+device = "cpu"
+# device = "mps"
+
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to(device)
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+
+image.save("astronaut_rides_horse.png")
