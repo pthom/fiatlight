6c47a8d61767ab131560920220ba216327bb9549
Pascal Thomet <pthomet@gmail.com>
2024-03-13 11:18:31 +0100

    fix issue / update next function

--- src/python/fiatlight/core/function_node.py
+++ src/python/fiatlight/core/function_node.py
@@ -63,6 +63,7 @@ class FunctionNode:
             src_output = self.function_with_gui.outputs_with_gui[link.src_output_idx]
             dst_input = link.dst_function_node.function_with_gui.input_of_name(link.dst_input_name)
             dst_input.value = src_output.data_with_gui.value
+            link.dst_function_node.function_with_gui.dirty = True
             link.dst_function_node.invoke_function()
 
     def save_user_inputs_to_json(self) -> JsonDict:
