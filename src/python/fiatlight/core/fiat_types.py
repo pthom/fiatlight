5d56a1d84af3a1a58d899b17c676561bdbcd82cf
Pascal Thomet <pthomet@gmail.com>
2024-03-20 15:07:09 +0100

    Add StrFunction

--- src/python/fiatlight/core/fiat_types.py
+++ src/python/fiatlight/core/fiat_types.py
@@ -3,6 +3,7 @@ from typing import Callable, Any, TypeAlias, TypeVar
 Function: TypeAlias = Callable[..., Any]
 VoidFunction = Callable[[], None]
 BoolFunction = Callable[[], bool]
+StrFunction = Callable[[], str]
 
 
 class Unspecified:
@@ -13,6 +14,9 @@ class Unspecified:
     def __init__(self, secret: int) -> None:
         if secret != 42:
             raise ValueError("UnspecifiedClass should not be instantiated directly, use the UnspecifiedValue constant")
+
+    def __str__(self) -> str:
+        return "Unspecified"
 
 
 UnspecifiedValue = Unspecified(42)
@@ -26,6 +30,9 @@ class Error:
     def __init__(self, secret: int) -> None:
         if secret != 42:
             raise ValueError("ErrorClass should not be instantiated directly, use the ErrorValue constant")
+
+    def __str__(self) -> str:
+        return "Error"
 
 
 ErrorValue = Error(42)
