46547dc4c87f377d04427ddc79f97d251e119ad3
Pascal Thomet <pthomet@gmail.com>
2024-04-20 15:31:17 +0200

    Add sandbox_node_group

---
+++ src/python/fiatlight/sandbox/sandbox_node_group.py
@@ -0,0 +1,40 @@
+from imgui_bundle import immapp, imgui_node_editor as ed, imgui_node_editor_ctx as edctx, imgui, ImVec2, ImVec4  # noqa
+
+
+def gui():
+    ed.begin("Node Editor")
+
+    pin_input_id = ed.PinId(200)
+    pin_output_id = ed.PinId(201)
+    link_id = ed.LinkId(202)
+
+    with edctx.begin_group_hint(ed.NodeId(20)):
+        with edctx.begin_node(ed.NodeId(40)):
+            imgui.text("Group Hint")
+            ed.group(ImVec2(200, 200))
+
+    with edctx.begin_node(ed.NodeId(1)):
+        with edctx.begin_pin(pin_input_id, ed.PinKind.input):
+            imgui.text("Pin 200")
+        imgui.text("Node 1")
+
+    with edctx.begin_node(ed.NodeId(2)):
+        with edctx.begin_pin(pin_output_id, ed.PinKind.output):
+            imgui.text("Pin 201")
+        imgui.text("Node 2")
+
+    with edctx.begin_node(ed.NodeId(3)):
+        imgui.text("Node 3")
+
+    ed.link(link_id, pin_input_id, pin_output_id, ImVec4(1.0, 1.0, 1.0, 1.0), 3.0)
+    # ed.flow(link_id, ed.FlowDirection.forward)
+
+    ed.end()
+
+
+def main():
+    immapp.run(gui, with_node_editor=True)
+
+
+if __name__ == "__main__":
+    main()
