fe8e3dec467edab6ceee81052aaa74e5e3c1fb9c
Pascal Thomet <pthomet@gmail.com>
2024-04-28 20:51:17 +0200

    Plots can now be resized inside Node Editor.

--- src/python/fiatlight/fiat_widgets/__init__.py
+++ src/python/fiatlight/fiat_widgets/__init__.py
@@ -2,7 +2,6 @@ from fiatlight.fiat_widgets import fiat_osd
 from fiatlight.fiat_widgets.misc_widgets import text_maybe_truncated, collapsible_button, button_with_disable_flag
 from fiatlight.fiat_widgets.fontawesome6_ctx_utils import fontawesome_6_ctx, icons_fontawesome_6
 from fiatlight.fiat_widgets.node_separator import node_separator, NodeSeparatorParams, NodeSeparatorOutput
-from fiatlight.fiat_widgets.widget_with_resize_handle import widget_with_resize_handle
 
 __all__ = [
     "fiat_osd",
@@ -14,5 +13,4 @@ __all__ = [
     "button_with_disable_flag",
     "icons_fontawesome_6",
     "fontawesome_6_ctx",
-    "widget_with_resize_handle",
 ]
