c923248a6a7fb043eeeeae3c72d65357c4648296
Pascal Thomet <pthomet@gmail.com>
2024-04-29 17:47:17 +0200

    add package_structure.md

--- src/python/fiatlight/demos/ai/demo_sdxl_toon_edges.py
+++ src/python/fiatlight/demos/ai/demo_sdxl_toon_edges.py
@@ -1,12 +1,11 @@
 import fiatlight
 from fiatlight.demos.ai.stable_diffusion_xl_wrapper import invoke_stable_diffusion_xl
-from fiatlight.demos.images.opencv_wrappers import oil_paint
 from fiatlight.demos.images.toon_edges import add_toon_edges
 from fiatlight.fiat_image import lut_channels_in_colorspace
 
 
 def main() -> None:
-    fiatlight.fiat_run_composition([invoke_stable_diffusion_xl, lut_channels_in_colorspace, add_toon_edges, oil_paint])
+    fiatlight.fiat_run_composition([invoke_stable_diffusion_xl, lut_channels_in_colorspace, add_toon_edges])
 
 
 if __name__ == "__main__":
