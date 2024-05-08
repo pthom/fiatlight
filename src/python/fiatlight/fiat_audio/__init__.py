455343d7ea6cf1d871361acf26708121dadef006
Pascal Thomet <pthomet@gmail.com>
2024-05-08 08:10:27 +0200

    fix audio _register_gui_factories

--- src/python/fiatlight/fiat_audio/__init__.py
+++ src/python/fiatlight/fiat_audio/__init__.py
@@ -8,8 +8,8 @@ def _register_gui_factories() -> None:
     from fiatlight.fiat_audio.sound_wave_player_gui import SoundWavePlayerGui
     from fiatlight.fiat_audio.audio_types_gui import register_audio_types_gui
 
-    prefix = "fiatlight.fiat_audio."
-    gui_factories().register_factory(prefix + "sound_wave.SoundWave", SoundWavePlayerGui)
+    prefix = "fiatlight.fiat_audio.audio_types."
+    gui_factories().register_factory(prefix + "SoundWave", SoundWavePlayerGui)
     register_audio_types_gui()
 
 
