91d362542457177e05fe69dad24c093d828daab3
Pascal Thomet <pthomet@gmail.com>
2024-05-09 09:52:49 +0200

    add simple_plot_gui_sandbox

--- src/python/fiatlight/fiat_kits/fiat_array/simple_plot_gui_sandbox.py
+++ src/python/fiatlight/fiat_kits/fiat_array/simple_plot_gui_sandbox.py
@@ -1,11 +1,15 @@
-"""A simple example of plotting a curve using fiatlight
-
-We use FloatMatrix_Dim2 (which is an alias for np.ndarray[ShapeDim2, AnyFloat]) to represent a 2D array of floats.
-We instruct fiatlight to display 2D arrays as plots, by calling present_float2_arrays_as_plot().
-"""
-from fiatlight.fiat_kits.fiat_array import FloatMatrix_Dim2
-from fiatlight.fiat_types import Float_0_100
 import fiatlight
+from fiatlight import fiat_array
+from fiatlight.fiat_types import Float_0_100
+import numpy as np
+import math
+
+
+def sin_wave() -> fiat_array.FloatMatrix_Dim2:
+    x = np.arange(0, 10, 0.1)
+    y = np.sin(x)
+    r = np.stack((x, y))
+    return r
 
 
 def make_spirograph_curve(
@@ -13,7 +17,7 @@ def make_spirograph_curve(
     radius_moving_circle: Float_0_100 = Float_0_100(3.48),
     pen_offset: Float_0_100 = Float_0_100(6.0),
     nb_turns: Float_0_100 = Float_0_100(23.0),
-) -> FloatMatrix_Dim2:
+) -> fiat_array.FloatMatrix_Dim2:
     """a spirograph-like curve"""
     import numpy as np
 
@@ -27,9 +31,20 @@ def make_spirograph_curve(
     return np.array([x, y])  # type: ignore
 
 
-def main() -> None:
-    fiatlight.fiat_run(make_spirograph_curve)
+def get_simple_values(x: float) -> fiat_array.FloatMatrix_Dim1:
+    r = []
+    for i in range(10):
+        r.append(math.cos(x**i))
+    return np.array(r)  # type: ignore
+
+
+def sandbox() -> None:
+    graph = fiatlight.FunctionsGraph()
+    graph.add_function(sin_wave)
+    graph.add_function(make_spirograph_curve)
+    graph.add_function(get_simple_values)
+    fiatlight.fiat_run_graph(graph)
 
 
 if __name__ == "__main__":
-    main()
+    sandbox()
