f2e2860431e0a7b72224bb391172a9593d8021f0
Pascal Thomet <pthomet@gmail.com>
2024-05-10 11:21:48 +0200

    fiat_audio_simple: classify as experimental

--- src/python/fiatlight/fiat_kits/__init__.py
+++ src/python/fiatlight/fiat_kits/__init__.py
@@ -1,5 +1,5 @@
-from fiatlight.fiat_kits import fiat_image, fiat_array, fiat_audio_simple
-
+from fiatlight.fiat_kits import fiat_image, fiat_array
+from fiatlight.fiat_kits.experimental import fiat_audio_simple
 
 __all__ = [
     "fiat_image",
