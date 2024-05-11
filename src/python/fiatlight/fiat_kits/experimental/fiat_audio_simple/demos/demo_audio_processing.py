9c81b1f8288bbcd3b30e1fe17895008958704689
Pascal Thomet <pthomet@gmail.com>
2024-05-12 00:22:16 +0200

    work on demo audio

--- src/python/fiatlight/fiat_kits/experimental/fiat_audio_simple/demos/demo_audio_processing.py
+++ src/python/fiatlight/fiat_kits/experimental/fiat_audio_simple/demos/demo_audio_processing.py
@@ -1,3 +1,5 @@
+# pip install librosa matplotlib numpy scipy pedalboard
+
 from fiatlight import FunctionWithGui
 from fiatlight.fiat_kits.experimental import fiat_audio_simple
 from imgui_bundle import hello_imgui, imgui_fig, ImVec2
@@ -63,12 +65,13 @@ def evaluate_note(wave: fiat_audio_simple.SoundWave) -> str:
     # Compute the error in cents
     error_cents = 1200 * np.log2(f0_mean / note_freq)
 
-    info = f"""
-    frequency: {f0_mean:.2f} Hz
-    note: {note}
-    solfege: {solfege}
-    error: {error_cents:.2f} cents
-           (100 cents is one semitone)
+    tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+
+    info = f"""frequency: {f0_mean:.2f} Hz
+note: {note}
+solfege: {solfege}
+error: {error_cents:.2f} cents (100 cents is one semitone)
+tempo: {tempo:.2f} BPM
         """
     return info
 
@@ -77,16 +80,16 @@ def evaluate_note(wave: fiat_audio_simple.SoundWave) -> str:
 evaluate_note.invoke_async = True  # type: ignore
 
 
-class ShowNoteGraph(FunctionWithGui):
+class ShowFundamentalFreqGraph(FunctionWithGui):
     # cf example here: https://librosa.org/doc/latest/generated/librosa.pyin.html#librosa.pyin
 
     # A matplotlib figure and axis to display the note graph
     fig: plt.Figure | None = None
-    ax: plt.Axes | None = None
     fig_size: ImVec2 | None = None
+    should_refresh_fig: bool = False
 
     def __init__(self) -> None:
-        super().__init__(self.f, "ShowNoteGraph")
+        super().__init__(self.f, "ShowFundamentalFreqGraph")
         # Our internal state GUI function will display the matplotlib figure
         self.internal_state_gui = self.internal_state_gui
         # Tell fiatlight to run this function asynchronously
@@ -102,13 +105,19 @@ class ShowNoteGraph(FunctionWithGui):
         if self.fig_size is None:
             self.fig_size = hello_imgui.em_to_vec2(20, 20)
 
-        imgui_fig.fig("##NoteGraph", self.fig, self.fig_size)
+        imgui_fig.fig("##NoteGraph", self.fig, self.fig_size, refresh_image=self.should_refresh_fig)
+        self.should_refresh_fig = False
 
     def _compute_note_graph(self, wave: fiat_audio_simple.SoundWave) -> None:
+        # this function will be called asynchronously!
+        # (see self.invoke_async = True in __init__)
         y = np.array(wave.wave)
         sr = wave.sample_rate
 
         # Pre-emphasis to increase the signal-to-noise ratio
+        # This is a high-pass filter that boosts the high frequencies
+        # (see https://en.wikipedia.org/wiki/Pre-emphasis)
+        # (Without this we may experience failures)
         y = np.append(y[0], y[1:] - 0.97 * y[:-1])
 
         # Estimate the fundamental frequency (f0) of the signal
@@ -117,73 +126,52 @@ class ShowNoteGraph(FunctionWithGui):
         )  # type: ignore
 
         times = librosa.times_like(f0, sr=sr)
-
         D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
-        self.fig, self.ax = plt.subplots()
-        img = librosa.display.specshow(D, x_axis="time", y_axis="log", ax=self.ax)
-        self.ax.set(title="pYIN fundamental frequency estimation")
-        self.fig.colorbar(img, ax=self.ax, format="%+2.f dB")
-        self.ax.plot(times, f0, label="f0", color="cyan", linewidth=3)
-        self.ax.legend(loc="upper right")
+
+        # Since this function is async, we shall not fill self.fig until it is fully ready
+        # Instead we work on a local fig
+        fig, ax = plt.subplots()
+        img = librosa.display.specshow(D, x_axis="time", y_axis="log", ax=ax)
+        ax.set(title="pYIN fundamental frequency estimation")
+        fig.colorbar(img, ax=ax, format="%+2.f dB")
+        ax.plot(times, f0, label="f0", color="cyan", linewidth=3)
+        ax.legend(loc="upper right")
+
+        # And copy it atomically when it is ready
+        self.fig = fig
+        self.should_refresh_fig = True
 
 
-# def evaluate_bpm(wave: fiat_audio_simple.SoundWave) -> float:
-#     """
-#     Estimate the tempo in beats per minute of a SoundWave.
-#     """
-#     y = np.array(wave.wave)
-#     sr = wave.sample_rate
-#     tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
-#     return tempo
-#
-#
-# def change_pitch(wave: fiat_audio_simple.SoundWave, semitones: float) -> fiat_audio_simple.SoundWave:
-#     """
-#     Change the pitch of a SoundWave by a number of semitones.
-#     """
-#     y = np.array(wave.wave)
-#     sr = wave.sample_rate
-#     y_shifted = librosa.effects.pitch_shift(y, sr, semitones)
-#     return fiat_audio_simple.SoundWave(wave=y_shifted, sample_rate=sr)
-#
-#
-# def add_echo(wave: fiat_audio_simple.SoundWave, delay: float, decay: float) -> fiat_audio_simple.SoundWave:
-#     """
-#     Add an echo effect to a SoundWave.
-#     Delay is in seconds, decay is the attenuation factor.
-#     """
-#     y = np.array(wave.wave)
-#     sr = wave.sample_rate
-#     # Create an echo filter
-#     delay_samples = int(delay * sr)
-#     echo_filter = np.zeros(delay_samples * 2)
-#     echo_filter[0] = 1
-#     echo_filter[delay_samples] = decay
-#     y_echo = np.convolve(y, echo_filter)[: len(y)]
-#     return fiat_audio_simple.SoundWave(wave=y_echo, sample_rate=sr)
+def add_chorus_effect(wave: fiat_audio_simple.SoundWave) -> fiat_audio_simple.SoundWave:
+    from pedalboard import Pedalboard, Chorus, Reverb
+
+    # Make a Pedalboard object, containing multiple audio plugins:
+    board = Pedalboard([Chorus(), Reverb(room_size=0.25)])
+
+    # Run the audio through our pedalboard:
+    effected = board(wave.wave, wave.sample_rate, reset=False)
+
+    return fiat_audio_simple.SoundWave(wave=effected, sample_rate=wave.sample_rate)  # type: ignore
 
 
-def make_440hz_wave(duration: float) -> fiat_audio_simple.SoundWave:
-    """
-    Create a 440 Hz sine wave SoundWave of a given duration.
-    """
-    sr = 44100
-    t = np.linspace(0, duration, int(sr * duration), endpoint=False)
-    y = np.sin(2 * np.pi * 440 * t)
-    return fiat_audio_simple.SoundWave(wave=y, sample_rate=sr)  # type: ignore
+add_chorus_effect.invoke_async = True  # type: ignore
 
 
 def sandbox() -> None:
     import fiatlight  # noqa
 
     graph = fiatlight.FunctionsGraph()
+
     graph.add_function(fiat_audio_simple.MicrophoneGui())
 
     graph.add_function(evaluate_note)
     graph.add_link("MicrophoneGui", "evaluate_note")
 
-    graph.add_function(ShowNoteGraph())
-    graph.add_link("MicrophoneGui", "ShowNoteGraph")
+    graph.add_function(ShowFundamentalFreqGraph())
+    graph.add_link("MicrophoneGui", "ShowFundamentalFreqGraph")
+
+    graph.add_function(add_chorus_effect)
+    graph.add_link("MicrophoneGui", "add_chorus_effect")
 
     fiatlight.fiat_run_graph(graph)
 
