30479ec7721626ae1ee8df99f72d3dc1ef895ada
Pascal Thomet <pthomet@gmail.com>
2024-05-12 14:31:21 +0200

    Add demo_pedal_board

---
+++ src/python/fiatlight/fiat_kits/experimental/fiat_audio_simple/demos/demo_pedalboard.py
@@ -0,0 +1,37 @@
+# type: ignore
+
+# pip install librosa matplotlib numpy scipy pedalboard
+
+from fiatlight.fiat_kits.experimental import fiat_audio_simple
+
+
+def add_chorus_effect(wave: fiat_audio_simple.SoundWave) -> fiat_audio_simple.SoundWave:
+    from pedalboard import Pedalboard, Chorus, Reverb
+
+    # Make a Pedalboard object, containing multiple audio plugins:
+    board = Pedalboard([Chorus(), Reverb(room_size=0.25)])
+
+    # Run the audio through our pedalboard:
+    effected = board(wave.wave, wave.sample_rate, reset=False)
+
+    return fiat_audio_simple.SoundWave(wave=effected, sample_rate=wave.sample_rate)  # type: ignore
+
+
+add_chorus_effect.invoke_async = True  # type: ignore
+
+
+def sandbox() -> None:
+    import fiatlight  # noqa
+
+    graph = fiatlight.FunctionsGraph()
+
+    graph.add_function(fiat_audio_simple.MicrophoneGui())
+
+    graph.add_function(add_chorus_effect)
+    graph.add_link("MicrophoneGui", "add_chorus_effect")
+
+    fiatlight.fiat_run_graph(graph)
+
+
+if __name__ == "__main__":
+    sandbox()
