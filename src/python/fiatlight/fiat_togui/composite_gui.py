5727391f55ecd20f05b6bc3b42e9cedc86f71a6d
Pascal Thomet <pthomet@gmail.com>
2024-05-14 22:55:20 +0200

    add composite_gui_demo

--- src/python/fiatlight/fiat_togui/composite_gui.py
+++ src/python/fiatlight/fiat_togui/composite_gui.py
@@ -191,37 +191,3 @@ class EnumWithGui(AnyDataWithGui[Enum]):
 
     def create_from_name(self, name: str) -> Enum:
         return self.enum_type[name]
-
-
-# ---------------------------- Sandbox ----------------------------
-def sandbox_optional() -> None:
-    from fiatlight import fiat_run_graph, FunctionsGraph
-
-    def foo(x: int | None) -> int:
-        if x is None:
-            return 0
-        else:
-            return x + 2
-
-    graph = FunctionsGraph.from_function_composition([foo])
-    fiat_run_graph(graph)
-
-
-def sandbox_enum() -> None:
-    from fiatlight import fiat_run_graph, FunctionsGraph
-
-    class MyEnum(Enum):
-        A = 1
-        B = 2
-        C = 3
-
-    def foo(x: MyEnum) -> int:
-        return x.value
-
-    graph = FunctionsGraph.from_function_composition([foo])
-    fiat_run_graph(graph)
-
-
-if __name__ == "__main__":
-    # sandbox_optional()
-    sandbox_enum()
---
+++ src/python/fiatlight/fiat_togui/composite_gui_demo.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+def sandbox_optional() -> None:
+    from fiatlight import fiat_run_graph, FunctionsGraph
+
+    def foo(x: int | None) -> int:
+        if x is None:
+            return 0
+        else:
+            return x + 2
+
+    graph = FunctionsGraph.from_function_composition([foo])
+    fiat_run_graph(graph)
+
+
+def sandbox_enum() -> None:
+    from fiatlight import fiat_run_graph, FunctionsGraph
+
+    class MyEnum(Enum):
+        A = 1
+        B = 2
+        C = 3
+
+    def foo(x: MyEnum) -> int:
+        return x.value
+
+    graph = FunctionsGraph.from_function_composition([foo])
+    fiat_run_graph(graph)
