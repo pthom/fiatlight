5f6c3ea036167961d1ea44ae71900b1bd7a8ba00
Pascal Thomet <pthomet@gmail.com>
2024-06-05 16:28:13 +0200

    doc

---
+++ src/python/fiatlight/sandbox/lissajou_s.py
@@ -0,0 +1,33 @@
+# Part 1: Standard Python code (without GUI)
+# ------------------------------------------
+import numpy as np
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+
+def lissajous_curve(freq1: float = 5.0, freq2: float = 4.0, delta: float = np.pi / 2, nb_periods: float = 1) -> Figure:
+    """Creates a Lissajous curve, and returns a Matplotlib figure."""
+    t = np.linspace(0, 2 * np.pi * nb_periods, 10_000)
+    x = np.sin(freq1 * t + delta)
+    y = np.sin(freq2 * t)
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    return fig
+
+
+# Part 2: Add a GUI to the code in a few seconds
+# ----------------------------------------------
+import fiatlight as fl
+
+# Options for widgets
+fl.add_custom_attrs(
+    lissajous_curve,
+    freq1__range=(0, 10),
+    freq2__range=(0, 10),
+    delta__range=(-np.pi, np.pi),
+    nb_periods__range=(0.1, 10),
+    nb_periods__edit_type="knob",
+)
+
+# Run the function interactively
+fl.run(lissajous_curve, app_name="Interactive Lissajou Curve")
