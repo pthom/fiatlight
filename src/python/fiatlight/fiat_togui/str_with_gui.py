4d4bbf4cff722a9fb5ca57326a16f720e05fb768
Pascal Thomet <pthomet@gmail.com>
2024-06-07 10:56:03 +0200

    update StrWithGui

--- src/python/fiatlight/fiat_togui/str_with_gui.py
+++ src/python/fiatlight/fiat_togui/str_with_gui.py
@@ -16,8 +16,15 @@ class StrPossibleCustomAttributes(PossibleCustomAttributes):
         self.add_explained_attribute(
             name="width_em",
             type_=float,
-            explanation="Initial width of the text input (in em unit). Can be saved if resizable is True",
+            explanation="Initial width of the single line text input (in em unit). Can be saved if resizable is True",
             default_value=15.0,
+        )
+        self.add_explained_attribute(
+            name="size_multiline_em",
+            type_=tuple,
+            explanation="Initial size of the multiline text input (in em unit)",
+            default_value=(60.0, 15.0),
+            tuple_types=(float, float),
         )
         self.add_explained_attribute(
             name="hint",
@@ -28,7 +35,7 @@ class StrPossibleCustomAttributes(PossibleCustomAttributes):
         self.add_explained_attribute(
             name="allow_multiline",
             type_=bool,
-            explanation="Whether the user can edit the string as multiline string in a popup",
+            explanation="Whether the user can edit the string as multiline string (when not in a function node)",
             default_value=False,
         )
         self.add_explained_attribute(
@@ -47,6 +54,7 @@ class StrWithGuiParams(BaseModel):
 
     # Saved parameters (user pref)
     width_em: float = 15.0  # single line editing width
+    size_multiline_em: tuple[float, float] = (60.0, 15.0)  # multiline editing size
 
     # Developer parameters (not saved)
     resizable: bool = True  # single line widget is resizable
@@ -60,18 +68,20 @@ class StrWithGui(AnyDataWithGui[str]):
     params: StrWithGuiParams
 
     _input_text_in_node: hello_imgui.InputTextData
-    _input_text_in_popup: hello_imgui.InputTextData
+    _input_text_classic: hello_imgui.InputTextData
 
     def __init__(self) -> None:
         super().__init__(str)
         self.params = StrWithGuiParams()
         self._input_text_in_node = hello_imgui.InputTextData("", multiline=False, size_em=ImVec2(15, 0))
-        self._input_text_in_popup = hello_imgui.InputTextData("", multiline=True, size_em=ImVec2(60, 15))
+        self._input_text_classic = hello_imgui.InputTextData("", multiline=False, size_em=ImVec2(15, 0))
         self.callbacks.on_change = self.on_change
         self.callbacks.edit = self.edit
         self.callbacks.edit_popup_possible = False
         self.callbacks.present_custom = self.present_custom
         self.callbacks.present_custom_popup_possible = True
+        self.callbacks.present_custom_collapsible = False
+        self.callbacks.edit_collapsible = False
         self.callbacks.default_value_provider = lambda: ""
         self.callbacks.save_gui_options_to_json = self.save_gui_options_to_json
         self.callbacks.load_gui_options_from_json = self.load_gui_options_from_json
@@ -79,7 +89,7 @@ class StrWithGui(AnyDataWithGui[str]):
 
     def on_change(self, value: str) -> None:
         self._input_text_in_node.text = value
-        self._input_text_in_popup.text = value
+        self._input_text_classic.text = value
 
     def on_custom_attrs_changed(self, custom_attrs: CustomAttributesDict) -> None:
         if "width_em" in custom_attrs:
@@ -88,6 +98,8 @@ class StrWithGui(AnyDataWithGui[str]):
             self.params.hint = custom_attrs["hint"]
         if "allow_multiline" in custom_attrs:
             self.params.allow_multiline = custom_attrs["allow_multiline"]
+            self.callbacks.present_custom_collapsible = self.params.allow_multiline
+            self.callbacks.edit_collapsible = self.params.allow_multiline
         if "resizable" in custom_attrs:
             self.params.resizable = custom_attrs["resizable"]
         self._update_internal_state_from_params()
@@ -146,15 +158,37 @@ class StrWithGui(AnyDataWithGui[str]):
                     self.params.width_em = self._input_text_in_node.size_em.x
                     if changed:
                         value = self._input_text_in_node.text
-                        self._input_text_in_popup.text = value
+                        self._input_text_classic.text = value
             else:
-                with imgui_ctx.begin_vertical("Prompt"):
-                    changed = hello_imgui.input_text_resizable("##StringPopup", self._input_text_in_popup)
-                    window_width = imgui.get_window_width()
-                    window_width_em = hello_imgui.pixel_size_to_em(window_width)
-                    self._input_text_in_popup.size_em.x = window_width_em - 3.0
+                with imgui_ctx.begin_vertical("StrEditPopup"):
+                    # Set editing mode for the input text
+                    self._input_text_classic.multiline = self.params.allow_multiline
+                    # Set size of the input text (depends on the mode)
+                    if self.params.allow_multiline:
+                        self._input_text_classic.size_em = ImVec2(*self.params.size_multiline_em)
+                    else:
+                        self._input_text_classic.size_em = ImVec2(self.params.width_em, 0)
+
+                    # Special case: if we are in a detached window, we need to adjust the width of the input text
+                    if fiatlight.is_rendering_in_fiatlight_detached_window():
+                        window_width = imgui.get_window_width()
+                        window_width_em = hello_imgui.pixel_size_to_em(window_width)
+                        self._input_text_classic.size_em.x = window_width_em - 3.0
+
+                    # Display the widget
+                    changed = hello_imgui.input_text_resizable("##StringPopup", self._input_text_classic)
+
+                    # update params if resized
+                    if self.params.allow_multiline:
+                        self.params.size_multiline_em = (
+                            self._input_text_classic.size_em.x,
+                            self._input_text_classic.size_em.y,
+                        )
+                    else:
+                        self.params.width_em = self._input_text_classic.size_em.x
+
                     if changed:
-                        value = self._input_text_in_popup.text
+                        value = self._input_text_classic.text
                         self._input_text_in_node.text = value
 
         return changed, value
