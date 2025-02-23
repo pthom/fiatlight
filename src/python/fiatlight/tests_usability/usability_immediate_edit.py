a11bbfa3e869a1bacb2b2d253ebc67814e165bb7
Pascal Thomet <pthomet@gmail.com>
2025-02-23 12:55:03 +0100

    doc / usability_immedit

--- src/python/fiatlight/tests_usability/usability_immediate_edit.py
+++ src/python/fiatlight/tests_usability/usability_immediate_edit.py
@@ -1,3 +1,78 @@
+"""Demonstrates how Fiatlight can be used as a versatile immediate widget
+for any kind of data (int, str, pydantic model, etc.), with a simple API,
+and a high level of customization.
+
+In this example, we interactively edit with fiatlight.immediate_edit:
+- simple int values (either with a slider and a knob)
+- a composed pydantic model (User) with a nested model (Address)
+
+---
+
+Note: we used the fiatlight command line tool to get info about the
+possible customization options for the immediate_edit function for int values:
+
+```bash
+> fiatlight gui int
+
+GUI type: int
+==============
+  A highly customizable int widget.
+
+  Available fiat attributes for IntWithGui:
+  --------------------------------------------------------------------------------
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | Name               | Type            | Default   | Explanation                               |
+  +====================+=================+===========+===========================================+
+  | range              | tuple[int, int] | (0, 10)   | Range of the integer                      |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | edit_type          | str             | input     | Type of the edit widget. Possible values: |
+  |                    |                 |           | slider, input, drag, knob,                |
+  |                    |                 |           | slider_and_minus_plus                     |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | format             | str             | %d        | Format string for the value               |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | width_em           | float           | 9.0       | Width of the widget in em                 |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | knob_size_em       | float           | 2.5       | Size of the knob in em                    |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | knob_steps         | int             | 10        | Number of steps in the knob               |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | knob_no_input      | bool            | True      | Disable text input on knobs and sliders   |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | slider_no_input    | bool            | False     | Disable text input on sliders             |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+  | slider_logarithmic | bool            | False     | Use a logarithmic scale for sliders       |
+  +--------------------+-----------------+-----------+-------------------------------------------+
+
+  Available fiat attributes for AnyDataWithGui Generic attributes:
+  --------------------------------------------------------------------------------
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | Name                | Type   | Default             | Explanation                                    |
+  +=====================+========+=====================+================================================+
+  |                     |        |                     | **Generic attributes**                         |
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | validator           | object | None                | Function to validate a parameter value: should |
+  |                     |        |                     | raise a ValueError if invalid, or return the   |
+  |                     |        |                     | value (possibly modified)                      |
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | label               | str    |                     | A label for the parameter. If empty, the       |
+  |                     |        |                     | function parameter name is used                |
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | tooltip             | str    |                     | An optional tooltip to be displayed            |
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | label_color         | ImVec4 | ImVec4(0.000000,    | The color of the label (will use the default   |
+  |                     |        | 0.000000, 0.000000, | text color if not provided)                    |
+  |                     |        | 1.000000)           |                                                |
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | edit_collapsible    | bool   | True                | If True, the edit GUI may be collapsible       |
+  +---------------------+--------+---------------------+------------------------------------------------+
+  | present_collapsible | bool   | True                | If True, the present GUI may be collapsible    |
+  +---------------------+--------+---------------------+------------------------------------------------+
+
+```
+
+"""
+
 from imgui_bundle import hello_imgui, imgui
 from pydantic import BaseModel, field_validator
 import fiatlight as fl
